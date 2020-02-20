fd9b3ea3fd019bfe96045b86c3a23ab96b3c415a
katawut.io <katawutu.io@gmail.com>
2020-02-21 01:57:00 +0700

    generate random password

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -33,6 +33,26 @@ export class AppComponent {
   }
 
   onButtonClick() {
-    this.password = "MY PASSWORD!!!";
+    const numbers = "1234567890";
+    const letters = "abcdefghijklmnopqrstuvwxyz";
+    const symbols = "!@#$%^&*()";
+
+    let validChars = "";
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeSymbol) {
+      validChars += symbols;
+    }
+
+    let generatePassword = "";
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+    this.password = generatePassword;
   }
 }
